b04288f0ca53c204574a92da959896974ea08612
Paul Chin Jr <pchinjr@Pauls-MBP.home>
2020-01-08 12:15:19 -0500

    new prayer

--- public/bundle.js
+++ public/bundle.js
@@ -1,2 +1,417 @@
-var app=function(){"use strict";function t(){}function e(t){return t()}function n(){return Object.create(null)}function o(t){t.forEach(e)}function r(t){return"function"==typeof t}function a(t,e){return t!=t?e==e:t!==e||t&&"object"==typeof t||"function"==typeof t}function c(t,e,n){t.insertBefore(e,n||null)}function s(t){t.parentNode.removeChild(t)}function u(t){return document.createElement(t)}function i(t){return document.createTextNode(t)}function f(){return i(" ")}let l;function d(t){l=t}function p(t){(function(){if(!l)throw new Error("Function called outside component initialization");return l})().$$.on_mount.push(t)}const m=[],$=[],g=[],h=[],y=Promise.resolve();let b=!1;function x(t){g.push(t)}function _(){const t=new Set;do{for(;m.length;){const t=m.shift();d(t),w(t.$$)}for(;$.length;)$.pop()();for(let e=0;e<g.length;e+=1){const n=g[e];t.has(n)||(n(),t.add(n))}g.length=0}while(m.length);for(;h.length;)h.pop()();b=!1}function w(t){t.fragment&&(t.update(t.dirty),o(t.before_update),t.fragment.p(t.dirty,t.ctx),t.dirty=null,t.after_update.forEach(x))}const v=new Set;function E(t,e){t.$$.dirty||(m.push(t),b||(b=!0,y.then(_)),t.$$.dirty=n()),t.$$.dirty[e]=!0}function C(a,c,s,u,i,f){const p=l;d(a);const m=c.props||{},$=a.$$={fragment:null,ctx:null,props:f,update:t,not_equal:i,bound:n(),on_mount:[],on_destroy:[],before_update:[],after_update:[],context:new Map(p?p.$$.context:[]),callbacks:n(),dirty:null};let g=!1;var h,y;$.ctx=s?s(a,m,(t,e,n=e)=>($.ctx&&i($.ctx[t],$.ctx[t]=n)&&($.bound[t]&&$.bound[t](n),g&&E(a,t)),e)):m,$.update(),g=!0,o($.before_update),$.fragment=u($.ctx),c.target&&(c.hydrate?$.fragment.l(function(t){return Array.from(t.childNodes)}(c.target)):$.fragment.c(),c.intro&&((h=a.$$.fragment)&&h.i&&(v.delete(h),h.i(y))),function(t,n,a){const{fragment:c,on_mount:s,on_destroy:u,after_update:i}=t.$$;c.m(n,a),x(()=>{const n=s.map(e).filter(r);u?u.push(...n):o(n),t.$$.on_mount=[]}),i.forEach(x)}(a,c.target,c.anchor),_()),d(p)}class A{$destroy(){var e,n;n=1,(e=this).$$.fragment&&(o(e.$$.on_destroy),e.$$.fragment.d(n),e.$$.on_destroy=e.$$.fragment=null,e.$$.ctx={}),this.$destroy=t}$on(t,e){const n=this.$$.callbacks[t]||(this.$$.callbacks[t]=[]);return n.push(e),()=>{const t=n.indexOf(e);-1!==t&&n.splice(t,1)}}$set(){}}function N(e){var n,o,r,a,l,d,p,m,$,g;return{c(){var t,c,s;n=u("h1"),o=i(e.message),r=f(),(a=u("h2")).textContent="The Lord Prayer",l=f(),(d=u("p")).innerHTML="<strong>Our Cage,</strong> who art thy savior,",p=f(),(m=u("p")).textContent="Nicolas by thy name",$=f(),(g=u("p")).textContent="The Lord of War will come,",t=n,c="class",null==(s="svelte-1ucbz36")?t.removeAttribute(c):t.setAttribute(c,s)},m(t,e){c(t,n,e),function(t,e){t.appendChild(e)}(n,o),c(t,r,e),c(t,a,e),c(t,l,e),c(t,d,e),c(t,p,e),c(t,m,e),c(t,$,e),c(t,g,e)},p(t,e){t.message&&function(t,e){e=""+e,t.data!==e&&(t.data=e)}(o,e.message)},i:t,o:t,d(t){t&&(s(n),s(r),s(a),s(l),s(d),s(p),s(m),s($),s(g))}}}function S(t,e,n){let{message:o}=e;return p(async()=>{let t=await(await fetch("/api")).json();n("message",o=t.msg),console.log("MESSAGE: ",o)}),t.$set=t=>{"message"in t&&n("message",o=t.message)},{message:o}}return new class extends A{constructor(t){super(),C(this,t,S,N,a,["message"])}}({target:document.body,props:{message:"...loading"}})}();
+
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.head.appendChild(r) })(window.document);
+var app = (function () {
+    'use strict';
+
+    function noop() { }
+    function add_location(element, file, line, column, char) {
+        element.__svelte_meta = {
+            loc: { file, line, column, char }
+        };
+    }
+    function run(fn) {
+        return fn();
+    }
+    function blank_object() {
+        return Object.create(null);
+    }
+    function run_all(fns) {
+        fns.forEach(run);
+    }
+    function is_function(thing) {
+        return typeof thing === 'function';
+    }
+    function safe_not_equal(a, b) {
+        return a != a ? b == b : a !== b || ((a && typeof a === 'object') || typeof a === 'function');
+    }
+
+    function append(target, node) {
+        target.appendChild(node);
+    }
+    function insert(target, node, anchor) {
+        target.insertBefore(node, anchor || null);
+    }
+    function detach(node) {
+        node.parentNode.removeChild(node);
+    }
+    function element(name) {
+        return document.createElement(name);
+    }
+    function text(data) {
+        return document.createTextNode(data);
+    }
+    function space() {
+        return text(' ');
+    }
+    function attr(node, attribute, value) {
+        if (value == null)
+            node.removeAttribute(attribute);
+        else
+            node.setAttribute(attribute, value);
+    }
+    function children(element) {
+        return Array.from(element.childNodes);
+    }
+    function custom_event(type, detail) {
+        const e = document.createEvent('CustomEvent');
+        e.initCustomEvent(type, false, false, detail);
+        return e;
+    }
+
+    let current_component;
+    function set_current_component(component) {
+        current_component = component;
+    }
+    function get_current_component() {
+        if (!current_component)
+            throw new Error(`Function called outside component initialization`);
+        return current_component;
+    }
+    function onMount(fn) {
+        get_current_component().$$.on_mount.push(fn);
+    }
+
+    const dirty_components = [];
+    const binding_callbacks = [];
+    const render_callbacks = [];
+    const flush_callbacks = [];
+    const resolved_promise = Promise.resolve();
+    let update_scheduled = false;
+    function schedule_update() {
+        if (!update_scheduled) {
+            update_scheduled = true;
+            resolved_promise.then(flush);
+        }
+    }
+    function add_render_callback(fn) {
+        render_callbacks.push(fn);
+    }
+    function flush() {
+        const seen_callbacks = new Set();
+        do {
+            // first, call beforeUpdate functions
+            // and update components
+            while (dirty_components.length) {
+                const component = dirty_components.shift();
+                set_current_component(component);
+                update(component.$$);
+            }
+            while (binding_callbacks.length)
+                binding_callbacks.pop()();
+            // then, once components are updated, call
+            // afterUpdate functions. This may cause
+            // subsequent updates...
+            for (let i = 0; i < render_callbacks.length; i += 1) {
+                const callback = render_callbacks[i];
+                if (!seen_callbacks.has(callback)) {
+                    callback();
+                    // ...so guard against infinite loops
+                    seen_callbacks.add(callback);
+                }
+            }
+            render_callbacks.length = 0;
+        } while (dirty_components.length);
+        while (flush_callbacks.length) {
+            flush_callbacks.pop()();
+        }
+        update_scheduled = false;
+    }
+    function update($$) {
+        if ($$.fragment) {
+            $$.update($$.dirty);
+            run_all($$.before_update);
+            $$.fragment.p($$.dirty, $$.ctx);
+            $$.dirty = null;
+            $$.after_update.forEach(add_render_callback);
+        }
+    }
+    const outroing = new Set();
+    function transition_in(block, local) {
+        if (block && block.i) {
+            outroing.delete(block);
+            block.i(local);
+        }
+    }
+
+    const globals = (typeof window !== 'undefined' ? window : global);
+    function mount_component(component, target, anchor) {
+        const { fragment, on_mount, on_destroy, after_update } = component.$$;
+        fragment.m(target, anchor);
+        // onMount happens before the initial afterUpdate
+        add_render_callback(() => {
+            const new_on_destroy = on_mount.map(run).filter(is_function);
+            if (on_destroy) {
+                on_destroy.push(...new_on_destroy);
+            }
+            else {
+                // Edge case - component was destroyed immediately,
+                // most likely as a result of a binding initialising
+                run_all(new_on_destroy);
+            }
+            component.$$.on_mount = [];
+        });
+        after_update.forEach(add_render_callback);
+    }
+    function destroy_component(component, detaching) {
+        if (component.$$.fragment) {
+            run_all(component.$$.on_destroy);
+            component.$$.fragment.d(detaching);
+            // TODO null out other refs, including component.$$ (but need to
+            // preserve final state?)
+            component.$$.on_destroy = component.$$.fragment = null;
+            component.$$.ctx = {};
+        }
+    }
+    function make_dirty(component, key) {
+        if (!component.$$.dirty) {
+            dirty_components.push(component);
+            schedule_update();
+            component.$$.dirty = blank_object();
+        }
+        component.$$.dirty[key] = true;
+    }
+    function init(component, options, instance, create_fragment, not_equal, prop_names) {
+        const parent_component = current_component;
+        set_current_component(component);
+        const props = options.props || {};
+        const $$ = component.$$ = {
+            fragment: null,
+            ctx: null,
+            // state
+            props: prop_names,
+            update: noop,
+            not_equal,
+            bound: blank_object(),
+            // lifecycle
+            on_mount: [],
+            on_destroy: [],
+            before_update: [],
+            after_update: [],
+            context: new Map(parent_component ? parent_component.$$.context : []),
+            // everything else
+            callbacks: blank_object(),
+            dirty: null
+        };
+        let ready = false;
+        $$.ctx = instance
+            ? instance(component, props, (key, ret, value = ret) => {
+                if ($$.ctx && not_equal($$.ctx[key], $$.ctx[key] = value)) {
+                    if ($$.bound[key])
+                        $$.bound[key](value);
+                    if (ready)
+                        make_dirty(component, key);
+                }
+                return ret;
+            })
+            : props;
+        $$.update();
+        ready = true;
+        run_all($$.before_update);
+        $$.fragment = create_fragment($$.ctx);
+        if (options.target) {
+            if (options.hydrate) {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                $$.fragment.l(children(options.target));
+            }
+            else {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                $$.fragment.c();
+            }
+            if (options.intro)
+                transition_in(component.$$.fragment);
+            mount_component(component, options.target, options.anchor);
+            flush();
+        }
+        set_current_component(parent_component);
+    }
+    class SvelteComponent {
+        $destroy() {
+            destroy_component(this, 1);
+            this.$destroy = noop;
+        }
+        $on(type, callback) {
+            const callbacks = (this.$$.callbacks[type] || (this.$$.callbacks[type] = []));
+            callbacks.push(callback);
+            return () => {
+                const index = callbacks.indexOf(callback);
+                if (index !== -1)
+                    callbacks.splice(index, 1);
+            };
+        }
+        $set() {
+            // overridden by instance, if it has props
+        }
+    }
+
+    function dispatch_dev(type, detail) {
+        document.dispatchEvent(custom_event(type, detail));
+    }
+    function append_dev(target, node) {
+        dispatch_dev("SvelteDOMInsert", { target, node });
+        append(target, node);
+    }
+    function insert_dev(target, node, anchor) {
+        dispatch_dev("SvelteDOMInsert", { target, node, anchor });
+        insert(target, node, anchor);
+    }
+    function detach_dev(node) {
+        dispatch_dev("SvelteDOMRemove", { node });
+        detach(node);
+    }
+    function attr_dev(node, attribute, value) {
+        attr(node, attribute, value);
+        if (value == null)
+            dispatch_dev("SvelteDOMRemoveAttribute", { node, attribute });
+        else
+            dispatch_dev("SvelteDOMSetAttribute", { node, attribute, value });
+    }
+    function set_data_dev(text, data) {
+        data = '' + data;
+        if (text.data === data)
+            return;
+        dispatch_dev("SvelteDOMSetData", { node: text, data });
+        text.data = data;
+    }
+    class SvelteComponentDev extends SvelteComponent {
+        constructor(options) {
+            if (!options || (!options.target && !options.$$inline)) {
+                throw new Error(`'target' is a required option`);
+            }
+            super();
+        }
+        $destroy() {
+            super.$destroy();
+            this.$destroy = () => {
+                console.warn(`Component was already destroyed`); // eslint-disable-line no-console
+            };
+        }
+    }
+
+    /* src/App.svelte generated by Svelte v3.12.1 */
+    const { console: console_1 } = globals;
+
+    const file = "src/App.svelte";
+
+    function create_fragment(ctx) {
+    	var h1, t0, t1, h2, t3, p, strong, t5;
+
+    	const block = {
+    		c: function create() {
+    			h1 = element("h1");
+    			t0 = text(ctx.message);
+    			t1 = space();
+    			h2 = element("h2");
+    			h2.textContent = "The Lord's Prayer";
+    			t3 = space();
+    			p = element("p");
+    			strong = element("strong");
+    			strong.textContent = "Our Father";
+    			t5 = text(", which art in Snake Eyes, \n  Nicolas be thy name;   \n  thy  Face/Off come; \n  thy Con-Air be done,\n  in earth as it is in Deadfall.\n  Give us this day our daily Ghost Rider.\n  And forgive him for Trespass,\n  as we forgave him for Windtalkers.\n  And lead us not into Adaptation.;\n  but deliver us from The Rock.\n  For thine is the Wicker Man,\n  the National Treasure");
+    			attr_dev(h1, "class", "svelte-1ucbz36");
+    			add_location(h1, file, 16, 0, 265);
+    			add_location(h2, file, 17, 0, 284);
+    			add_location(strong, file, 18, 3, 314);
+    			add_location(p, file, 18, 0, 311);
+    		},
+
+    		l: function claim(nodes) {
+    			throw new Error("options.hydrate only works if the component was compiled with the `hydratable: true` option");
+    		},
+
+    		m: function mount(target, anchor) {
+    			insert_dev(target, h1, anchor);
+    			append_dev(h1, t0);
+    			insert_dev(target, t1, anchor);
+    			insert_dev(target, h2, anchor);
+    			insert_dev(target, t3, anchor);
+    			insert_dev(target, p, anchor);
+    			append_dev(p, strong);
+    			append_dev(p, t5);
+    		},
+
+    		p: function update(changed, ctx) {
+    			if (changed.message) {
+    				set_data_dev(t0, ctx.message);
+    			}
+    		},
+
+    		i: noop,
+    		o: noop,
+
+    		d: function destroy(detaching) {
+    			if (detaching) {
+    				detach_dev(h1);
+    				detach_dev(t1);
+    				detach_dev(h2);
+    				detach_dev(t3);
+    				detach_dev(p);
+    			}
+    		}
+    	};
+    	dispatch_dev("SvelteRegisterBlock", { block, id: create_fragment.name, type: "component", source: "", ctx });
+    	return block;
+    }
+
+    function instance($$self, $$props, $$invalidate) {
+    	let { message } = $$props;
+      onMount(async () => {
+        let data = await (await fetch('/api')).json();
+        $$invalidate('message', message = data.msg);
+        console.log('MESSAGE: ', message);
+      });
+
+    	const writable_props = ['message'];
+    	Object.keys($$props).forEach(key => {
+    		if (!writable_props.includes(key) && !key.startsWith('$$')) console_1.warn(`<App> was created with unknown prop '${key}'`);
+    	});
+
+    	$$self.$set = $$props => {
+    		if ('message' in $$props) $$invalidate('message', message = $$props.message);
+    	};
+
+    	$$self.$capture_state = () => {
+    		return { message };
+    	};
+
+    	$$self.$inject_state = $$props => {
+    		if ('message' in $$props) $$invalidate('message', message = $$props.message);
+    	};
+
+    	return { message };
+    }
+
+    class App extends SvelteComponentDev {
+    	constructor(options) {
+    		super(options);
+    		init(this, options, instance, create_fragment, safe_not_equal, ["message"]);
+    		dispatch_dev("SvelteRegisterComponent", { component: this, tagName: "App", options, id: create_fragment.name });
+
+    		const { ctx } = this.$$;
+    		const props = options.props || {};
+    		if (ctx.message === undefined && !('message' in props)) {
+    			console_1.warn("<App> was created without expected prop 'message'");
+    		}
+    	}
+
+    	get message() {
+    		throw new Error("<App>: Props cannot be read directly from the component instance unless compiling with 'accessors: true' or '<svelte:options accessors/>'");
+    	}
+
+    	set message(value) {
+    		throw new Error("<App>: Props cannot be set directly on the component instance unless compiling with 'accessors: true' or '<svelte:options accessors/>'");
+    	}
+    }
+
+    let message = '...loading';
+
+    const app = new App({
+      target: document.body,
+      props: {
+        message
+      }
+    });
+
+    return app;
+
+}());
 //# sourceMappingURL=bundle.js.map
--- src/App.svelte
+++ src/App.svelte
@@ -15,7 +15,17 @@
 </style>
 
 <h1>{message}</h1>
-<h2>The Lord Prayer</h2>
-<p><strong>Our Cage,</strong> who art thy savior,</p>
-<p>Nicolas by thy name</p>
-<p>The Lord of War will come,</p>
+<h2>The Lord's Prayer</h2>
+<p><strong>Our Father</strong>, which art in Snake Eyes, 
+  Nicolas be thy name;   
+  thy  Face/Off come; 
+  thy Con-Air be done,
+  in earth as it is in Deadfall.
+  Give us this day our daily Ghost Rider.
+  And forgive him for Trespass,
+  as we forgave him for Windtalkers.
+  And lead us not into Adaptation.;
+  but deliver us from The Rock.
+  For thine is the Wicker Man,
+  the National Treasure
+</p>
